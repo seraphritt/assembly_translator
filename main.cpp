8d959d761dbd900ef8ea6a8793bffa6d484f32d7
seraphritt <isak369@hotmail.com>
2023-10-03 15:03:00 -0300

    trying to read file

--- main.cpp
+++ main.cpp
@@ -14,7 +14,18 @@ typedef vector<tuple<string, int, string>> instr_table_type;
 table_type symbols_table;
 // instrução, num maximo de argumentos, OP code
 instr_table_type instr_table = {make_tuple("ADD", 2, "01"), make_tuple("SUB", 2, "02"), make_tuple("MUL", 2, "03"), make_tuple("DIV", 2, "04"), make_tuple("JMP", 2, "05"), make_tuple("JMPN", 2, "06"), make_tuple("JMPP", 2, "07"), make_tuple("JMPZ", 2, "08"), make_tuple("COPY", 3, "09"), make_tuple("LOAD", 2, "10"), make_tuple("STORE", 2, "11"), make_tuple("INPUT", 2, "12"), make_tuple("OUTPUT", 2, "13"), make_tuple("STOP", 1, "14")};
-
+int contador_linha = 1;
+int contador_posicao = 0;
+bool findInIntrTable(string instr, int posit){
+    for(auto [X, Y, Z]: instr_table){
+        if(X == instr){
+            contador_posicao = contador_posicao + Y;
+            return true;
+        }
+    }
+    // retorna falso e irá procurar na tabela de diretivas
+    return false;
+}
 bool findInSymbolsTable(string label, int posit){
     for(auto [X, Y]: symbols_table ){
         if(X == label){
@@ -50,20 +61,30 @@ vector<string> readFile(string file_name){
     vector<string> operacao;
     vector<string> operandos;
     // comentarios serão ignorados
-    string token;
     ifstream inFile; // inFile é o arquivo de leitura dos dados
-    inFile.open(file_name, ios::in); // abre o arquivo para leitura
-    if (! inFile)
+    inFile.open(file_name);
+    if (!inFile)
     {
         cout << "Arquivo codigo.asm nao pode ser aberto" << endl;
         abort();
     }
-    while(inFile >> token){
-        cout << token << endl;
+    while(inFile){
+        string line;
+        string token;
+        getline(inFile, line);
+        cout << line;
         if(token[token.size() - 1] == ':'){
-            if(findInSymbolsTable(token, 0)){
+            if(findInSymbolsTable(token, contador_posicao)){
                 cout << "Erro semantico: rotulo redefinido na linha " << 0;
                 // retorna erro dizendo que na linha tal achou: redefinição de rótulo (semântico)
+            }
+        }
+        else{
+            if(findInIntrTable(token, contador_posicao)){
+                cout << "Achou" << endl;
+            }else{
+                cout << "Procurar na tabela de diretivas" << endl;
+                // procurar na tabela de diretivas
             }
         }
     }
@@ -76,10 +97,7 @@ vector<string> readFile(string file_name){
 }
 int main()
 {
-    vector<string> ret = readFile("codigo.asm");
-    for (string i : ret) {
-        cout << i << endl;
-    }
+    readFile("codigo.asm");
     for (auto [X, Y, Z] : instr_table){
         cout << X << " " << Y << " " << Z << endl;
     }
