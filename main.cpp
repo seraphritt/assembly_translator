affd282fc5cc41c365333a271c04d25ea4e3ae72
seraphritt <isak369@hotmail.com>
2023-10-14 01:01:23 -0300

    modificando main.cpp para colocar tupla na instr_and_operands.

--- main.cpp
+++ main.cpp
@@ -8,19 +8,20 @@
 //version: g++ (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0
 //using C++17
 using namespace std;
-vector<string> instr_and_operandos;
+vector<tuple<string, string, string>> instr_and_operandos;
 typedef vector<tuple<string, int>> table_type;
 typedef vector<tuple<string, int, string>> instr_table_type;
 table_type symbols_table;
 vector<string> dire_table = {"SPACE", "CONST", "SECAO DATA", "SECAO TEXT", "SECAO", "DATA", "TEXT"}; // diretivas
+vector<string> arquivo_obj;
 // instru��o, num maximo de argumentos, OP code
 vector<string> conteudo; // vetor que guarda o que ser� escrito no arquivo objeto
 instr_table_type instr_table = {make_tuple("ADD", 2, "01"), make_tuple("SUB", 2, "02"), make_tuple("MUL", 2, "03"), make_tuple("DIV", 2, "04"), make_tuple("JMP", 2, "05"), make_tuple("JMPN", 2, "06"), make_tuple("JMPP", 2, "07"), make_tuple("JMPZ", 2, "08"), make_tuple("COPY", 3, "09"), make_tuple("LOAD", 2, "10"), make_tuple("STORE", 2, "11"), make_tuple("INPUT", 2, "12"), make_tuple("OUTPUT", 2, "13"), make_tuple("STOP", 1, "14")};
-int contador_linha = 1;
+int contador_linha = 0;
 int contador_posicao = 0;
 
 
-vector<string> splitOperands(string operands){
+vector<string> splitOperands_comma(string operands){
     vector<string> result;
 
     istringstream ss(operands);
@@ -33,26 +34,61 @@ vector<string> splitOperands(string operands){
     return result;
 }
 
+vector<string> splitOperands_space(string operands){
+    vector<string> result;
+
+    istringstream ss(operands);
+    string element;
+
+    while (getline(ss, element, ' ')) {
+        result.push_back(element);
+    }
+
+    return result;
+}
+
 bool findInIntrTable(string instr, int posit){
     int tam = 0;
     int qtd = 0;
     if(instr != "\0"){
         istringstream a(instr); // tipo para usar o getline() e separar em espa�os
-        string s;
-        cout << "instr" << instr << endl;
-         while (getline( a, s, ' ') ) {
-            if((s != "")){
-                qtd += 1;
-                cout << s << endl;
-                instr_and_operandos.push_back(s);
-                tam = instr_and_operandos.size();
+        vector<string> s;
+        string ss;
+        cout << "instr: " << instr << endl;
+        if(instr.find("COPY") == string::npos){ // se não for copy, separa os operandos por espaço (só tem 1 operando) ou 0 no caso do stop e space
+            while (getline( a, ss, ' ') ) {
+                if((ss != "" && ss != "SECAO" && ss != "DATA" && ss != "TEXT")){
+                    s.push_back(ss);
+                    qtd += 1;
+                    cout << ss << endl;
+                    tam = instr_and_operandos.size();
+                }
             }
+            if(s.size() == 1){
+                instr_and_operandos.push_back(make_tuple(s[0], "", ""));
+            }else if(s.size() == 2){
+                instr_and_operandos.push_back(make_tuple(s[0], s[1], ""));
+            }
+
+        }else{
+              // se for copy, separa por vírgula
+            ss = splitOperands_space(instr)[1];
+            while (getline( a, ss, ',') ) {
+                if((ss != "" && ss != "SECAO" && ss != "DATA" && ss != "TEXT")){
+                    s.push_back(ss);
+                    cout << ss << endl;
+                    tam = instr_and_operandos.size();
+                }
+            }qtd += 3;
+            instr_and_operandos.push_back(make_tuple("COPY", s[0], s[1]));
         }
 
-        for(auto [X, Y, Z]: instr_table){
-            if(X == instr_and_operandos[tam - qtd]){
-                contador_posicao = contador_posicao + Y;
-                return true;
+        if(instr_and_operandos.size() != 0){
+            for(auto [X, Y, Z]: instr_table){
+                if(X == get<0>(instr_and_operandos[instr_and_operandos.size() - 1])){
+                    contador_posicao = contador_posicao + Y;
+                    return true;
+                }
             }
         }
     }
@@ -73,6 +109,7 @@ bool findInDireTable(string dire){
             }
         }
     }
+
     for(auto X: dire_table){
         if(dire1 != ""){
             if(X == dire1){
@@ -221,71 +258,15 @@ void readFile(string file_name){
 void secondPass(string file_name){
     contador_linha = 1; // zerando o contador_linha
     contador_posicao = 0; // zerando o contador_posicao
+    for(int i = 0; i < instr_and_operandos.size(); i++){ // se for uma operação, olha o operando. se o operando for um símbolo, procura na tabela de símbolos
+        cout << "operacoes " << get<0>(instr_and_operandos[i]) << endl;
 
-    // imprimindo instruções e operações e tabela de símbolos
-    // na segunda passagem olhamos apenas para as operações e os operandos, apenas para ajudar a debugar
+    // TODO: todos os operandos que estao em "instr_and_operandos" sao simbolos?
+    // Sim, todo operando é um símbolo já que não existe imediato
+    // Apenas na const e space o operando deve ser um número, mas a const e space são diretivas
+    // se nao sao, eles sempre irao ser numeros?
+    // e necessario criar um if para nao gerar erro quando o operando nao for simbolo
 
-    for(auto X : instr_and_operandos){
-        cout << "INSTR AND OPERANDOS: " << X << endl;
-    }
-
-    // for(auto [X, Y]: symbols_table){
-    //     cout << "TABELA DE SIMBOLOS: " << "X: " << X << " Y: " << Y << endl;
-    // }
-    bool achou_instr;
-    bool achou_dire;
-    bool is_operand = false; // primeiro vem a operação e depois o operando
-    for(auto W : instr_and_operandos){ // se for uma operação, olha o operando. se o operando for um símbolo, procura na tabela de símbolos
-        achou_instr = false;
-        achou_dire = false;
-        if(is_operand){
-            vector<std::string> split_operands = splitOperands(W);
-
-            for(const string & operand : split_operands){
-                cout << "OPERANDO: " << operand << endl;
-
-                // TODO: todos os operandos que estao em "instr_and_operandos" sao simbolos?
-                // Sim, todo operando é um símbolo já que não existe imediato
-                // Apenas na const e space o operando deve ser um número, mas a const e space são diretivas
-                // se nao sao, eles sempre irao ser numeros?
-                // e necessario criar um if para nao gerar erro quando o operando nao for simbolo
-                bool found_operand = false;
-                for(auto symbol : symbols_table){ // Procura operando na TS
-                    if(get<0>(symbol) == operand){
-                        found_operand = true;
-                        break;
-                    }
-                }
-
-                if(!found_operand){ // se não encontrou o operando na TS gera um erro
-                    cout << "ERRO: SIMBOLO INDEFINIDO" << endl;
-                    break;
-                }
-            }
-
-            is_operand = false;
-            continue;
-        }
-        for(auto [X, Y, Z] : instr_table){
-            if(W == X){ // procura instrucao na tabela de instrucoes
-                        // se não achar, procura na tabela de diretivas
-                is_operand = true; // se existir na tabela de intrucoes a proxima string sera um operando
-                achou_instr = true;
-                break;
-            }
-        }
-        if((!achou_instr) & (!is_operand)){ // procurar na tabela de diretivas
-            for(auto A: dire_table){
-                if(W == A){
-                    achou_dire = true;
-                    cout << "DIRETIVA: " << A << endl;
-                }
-            }
-            if(!achou_dire){
-                cout << "ERRO: OPERACAO NAO IDENTIFICADA" << endl;
-            }
-        }
-    }
 
     // PSEUDOCODIGO
     // Contador_posição = 0
@@ -310,6 +291,7 @@ void secondPass(string file_name){
     //             Contador_posição = valor retornado pela subrotina
     //         Senão: Erro, operação não identificada
     //     Contador_linha = contador_linha + 1
+    }
 }
 int main()
 {
