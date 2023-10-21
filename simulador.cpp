1fcb1b79931c1214d8f3b9d0603c75eb2a6bb233
seraphritt <isak369@hotmail.com>
2023-10-21 18:56:09 -0300

    adicionando todas as instruções no simulador.

--- simulador.cpp
+++ simulador.cpp
@@ -15,7 +15,6 @@ void runObjCode(){
     vector<long long int> refs;
     vector<long long int>::iterator it;
     long long int op_a = 0;
-    long long int op_b = 0;
     int i = 0;
     while(i < code.size()){
         cout << "i:" << i << endl;
@@ -24,43 +23,43 @@ void runObjCode(){
             cout << "min: " << *it << endl;
         }
         if(code[i] == "01"){ // add
-            cout << "PC <- " << i << " ";
             refs.push_back(stoi(code[i+1]));
             op_a = stoi(code[stoi(code[i+1])]);
             acc += op_a;
             // cout << "<DEBUG> A: " << op_a << endl;
-            cout << "ACC: " << acc << endl;
             i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
             continue;
         }
         else if(code[i] == "02"){ // sub
             refs.push_back(stoi(code[i+1]));
-            cout << "PC <- " << i << " ";
             op_a = stoi(code[stoi(code[i+1])]);
             acc -= op_a;
             // cout << "<DEBUG> A: " << op_a << endl;
-            cout << "ACC: " << acc << endl;
             i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
             continue;
         }
         else if(code[i] == "03"){ // mul
             refs.push_back(stoi(code[i+1]));
-            cout << "PC <- " << i << " ";
             op_a = stoi(code[stoi(code[i+1])]);
             acc *= op_a;
             // cout << "<DEBUG> A: " << op_a << endl;
-            cout << "ACC: " << acc << endl;
             i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
             continue;
         }
         else if(code[i] == "04"){ // div
             refs.push_back(stoi(code[i+1]));
-            cout << "PC <- " << i << " ";
             op_a = stoi(code[stoi(code[i+1])]);
             acc = acc/op_a;
             // cout << "<DEBUG> A: " << op_a << endl;
-            cout << "ACC: " << acc << endl;
             i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
             continue;
         }
         else if(code[i] == "05"){ // jmp
@@ -76,9 +75,10 @@ void runObjCode(){
                 cout << "ACC: " << acc << endl;
                 continue;
             }
+            i += 2;
             cout << "PC <- " << i << " ";
             cout << "ACC: " << acc << endl;
-            i += 1;
+            continue;
         }
         else if(code[i] == "07"){ // jmpp
             if(acc>0){
@@ -87,9 +87,62 @@ void runObjCode(){
                 cout << "ACC: " << acc << endl;
                 continue;
                 }
+            i += 2;
             cout << "PC <- " << i << " ";
             cout << "ACC: " << acc << endl;
-            i += 1;
+            continue;
+        }
+        else if(code[i] == "08"){ // jmpz
+            if(acc==0){
+                i = stoi(code[i + 1]);
+                cout << "PC <- " << i << " ";
+                cout << "ACC: " << acc << endl;
+                continue;
+                }
+            i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
+            continue;
+        }
+        else if(code[i] == "09"){ // copy
+            op_a = stoi(code[stoi(code[i+1])]); // operando A
+            code[stoi(code[i+2])] = op_a; // pega o contéudo do operando a e coloca no operando b
+            i += 3;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
+            continue;
+        }
+        else if(code[i] == "10"){ // load
+            acc = stoi(code[stoi(code[i+1])]);
+            i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
+            continue;
+        }
+        else if(code[i] == "11"){ // store
+            code[stoi(code[i+1])] = acc;
+            i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
+            continue;
+        }
+        else if(code[i] == "12"){ // input
+            long long int input = 0;
+            cin >> input;
+            code[stoi(code[i+1])] = input;
+            i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc << endl;
+            continue;
+        }
+        else if(code[i] == "13"){ // output
+            i += 2;
+            cout << "PC <- " << i << " ";
+            cout << "ACC: " << acc;
+            cout << "SAIDA: " << code[stoi(code[i+1])];
+        }
+        else if(code[i] == "14"){ // stop
+            break;
         }
     }
 }
